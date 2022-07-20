3fd6b227e18f8f8db6c69fd04b8c465ebc102f18
MotivDesktop1 <calebw@motiv.io>
2022-07-20 17:06:17 +1200

    4

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ window.onload = init
 function init() {
     const map = new ol.Map({
         view: new ol.View({
-            centre:[0,0],
+            center:[0,0],
             zoom : 2
         }),
         layers: [
